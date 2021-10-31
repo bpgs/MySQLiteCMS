1b6a020ee62ad5ba6d5ddc519385d20c5744064e
BP GS <awardfan@bpgs.de>
2021-10-31 21:46:21 +0100

    TinyMCE 4.3.8

--- cms/config/definitions.conf.php
+++ cms/config/definitions.conf.php
@@ -8,7 +8,9 @@ define('JQUERY_UI_HANDLER', STATIC_URL.'js/jquery_ui_handler.js');
 define('BOOTSTRAP', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
 define('BOOTSTRAP_CSS', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
 
-define('WYSIWYG_EDITOR', '//cdn.tinymce.com/4.5/tinymce.min.js');
+define('WYSIWYG_EDITOR', BASE_URL . '/static/js/tinymce-4.3.8-min.js');
+// define('WYSIWYG_EDITOR', '//cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js');
+// define('WYSIWYG_EDITOR', '//cdn.tinymce.com/4.5/tinymce.min.js');
 define('WYSIWYG_EDITOR_INIT', BASE_URL . 'static/js/wysiwyg_init.js');
 
 define('VALID_URL_CHARACTERS', '/^[a-zA-Z0-9._\-\/]+$/');
