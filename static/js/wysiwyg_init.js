b1c363d6b66073683aee060b02b936bffd7a00f3
BP GS <awardfan@bpgs.de>
2021-10-31 22:51:38 +0100

    Activation of Table Plugin

--- static/js/wysiwyg_init.js
+++ static/js/wysiwyg_init.js
@@ -1,14 +1,14 @@
 tinymce.init({
    selector: "#content",
-   menubar:false,
+   menubar:'table',
    statusbar: false,
    entity_encoding : "raw",
    plugins: [
-        "advlist autolink lists link image anchor code"
+        "advlist autolink lists link image anchor code table"
     ],
    target_list:false,
     
-   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code",
+   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code |",
    content_css : "../static/css/wysiwyg.css"
 
 });
