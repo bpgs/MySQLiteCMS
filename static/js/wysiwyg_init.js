95708e1dffb4aa7640cdbaadf6bed0be09b15f84
BP GS <awardfan@bpgs.de>
2021-11-02 21:30:15 +0100

    Spellcheck and Textcolor

--- static/js/wysiwyg_init.js
+++ static/js/wysiwyg_init.js
@@ -1,14 +1,15 @@
 tinymce.init({
    selector: "#content",
    menubar:'table',
+   browser_spellcheck: true,
    statusbar: false,
    entity_encoding : "raw",
    plugins: [
-        "advlist autolink lists link image anchor code table"
+        "advlist anchor autolink code image link lists table textcolor"
     ],
    target_list:false,
     
-   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code |",
+   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code | forecolor  backcolor",
    content_css : "../static/css/wysiwyg.css"
 
 });
