8ed931417472345cd50839af3bb42411df3198ab
BP GS <awardfan@bpgs.de>
2021-11-25 12:36:06 +0100

    function stripslashes_deep completely deactivated

--- cms/includes/functions.inc.php
+++ cms/includes/functions.inc.php
@@ -9,15 +9,15 @@ spl_autoload_register(function($class_name)
 
 // if(get_magic_quotes_gpc())
 // {
-  function stripslashes_deep($value)
-   {
-    $value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
-    return $value;
-   }
-  $_POST = array_map('stripslashes_deep', $_POST);
-  $_GET = array_map('stripslashes_deep', $_GET);
-  $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-  $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+//  function stripslashes_deep($value)
+//   {
+//    $value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+//    return $value;
+//   }
+//  $_POST = array_map('stripslashes_deep', $_POST);
+//  $_GET = array_map('stripslashes_deep', $_GET);
+//  $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+//  $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
 // }
 
 function showme($what)
