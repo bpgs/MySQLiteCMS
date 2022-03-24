d6a2b2e2dca1eb83d1b2a3e83484f48ca797aead
BP GS <awardfan@bpgs.de>
2022-03-24 12:10:01 +0100

    `FILE_DIR` added

--- cms/config/definitions.conf.php
+++ cms/config/definitions.conf.php
@@ -3,6 +3,7 @@
 define('JQUERY', '//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
 define('JQUERY_UI', '//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js');
 define('JQUERY_UI_CSS', '//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css');
+// Die folgende Definition wird nicht benutzt
 define('JQUERY_UI_HANDLER', STATIC_URL.'js/jquery_ui_handler.js');
 
 define('BOOTSTRAP', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
@@ -15,6 +16,8 @@ define('WYSIWYG_EDITOR_INIT', BASE_URL . 'static/js/wysiwyg_init.js');
 
 define('VALID_URL_CHARACTERS', '/^[a-zA-Z0-9._\-\/]+$/');
 define('MEDIA_DIR', 'static/media/');
+// Neu
+define('FILE_DIR', 'static/files/');
 define('IMAGE_IDENTIFIER', 'photo');
 define('CATEGORY_IDENTIFIER', 'category:');
 define('AMPERSAND_REPLACEMENT', ':AMP:');
